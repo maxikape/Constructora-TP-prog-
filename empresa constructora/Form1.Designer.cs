8a94294f46fbbccd983c4cbbf115251b818287f0
cape cape <cape20@hotmail.es>
2020-03-25 13:10:30 -0300

    terminado

---
+++ empresa constructora/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace empresa_constructora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtHoras = new System.Windows.Forms.TextBox();
+            this.TxtBasico = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtLegajo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CboxCategoria = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbll = new System.Windows.Forms.Label();
+            this.LblNetoaPagar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtHoras
+            // 
+            this.TxtHoras.Location = new System.Drawing.Point(128, 171);
+            this.TxtHoras.Name = "TxtHoras";
+            this.TxtHoras.Size = new System.Drawing.Size(100, 20);
+            this.TxtHoras.TabIndex = 3;
+            // 
+            // TxtBasico
+            // 
+            this.TxtBasico.Location = new System.Drawing.Point(128, 209);
+            this.TxtBasico.Name = "TxtBasico";
+            this.TxtBasico.Size = new System.Drawing.Size(100, 20);
+            this.TxtBasico.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(232, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 33);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Liquidación de Sueldos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Horas Trabajadas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sueldo Basico";
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(128, 136);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(100, 20);
+            this.TxtNombre.TabIndex = 8;
+            // 
+            // TxtLegajo
+            // 
+            this.TxtLegajo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.TxtLegajo.Location = new System.Drawing.Point(128, 97);
+            this.TxtLegajo.Name = "TxtLegajo";
+            this.TxtLegajo.Size = new System.Drawing.Size(100, 20);
+            this.TxtLegajo.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nombre y Apellido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "N° de Legajo";
+            // 
+            // CboxCategoria
+            // 
+            this.CboxCategoria.FormattingEnabled = true;
+            this.CboxCategoria.Items.AddRange(new object[] {
+            "CategoriaA",
+            "CategoriaB",
+            "CategoriaC"});
+            this.CboxCategoria.Location = new System.Drawing.Point(128, 256);
+            this.CboxCategoria.Name = "CboxCategoria";
+            this.CboxCategoria.Size = new System.Drawing.Size(100, 21);
+            this.CboxCategoria.TabIndex = 12;
+            this.CboxCategoria.Text = "SELECCIONAR";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Seleccione la Categoria";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 36);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Calcular Sueldo a Pagar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbll
+            // 
+            this.lbll.AutoSize = true;
+            this.lbll.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbll.Location = new System.Drawing.Point(402, 171);
+            this.lbll.Name = "lbll";
+            this.lbll.Size = new System.Drawing.Size(114, 20);
+            this.lbll.TabIndex = 24;
+            this.lbll.Text = "Neto a Pagar";
+            // 
+            // LblNetoaPagar
+            // 
+            this.LblNetoaPagar.AutoSize = true;
+            this.LblNetoaPagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNetoaPagar.Location = new System.Drawing.Point(563, 171);
+            this.LblNetoaPagar.Name = "LblNetoaPagar";
+            this.LblNetoaPagar.Size = new System.Drawing.Size(0, 20);
+            this.LblNetoaPagar.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblNetoaPagar);
+            this.Controls.Add(this.lbll);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.CboxCategoria);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtLegajo);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtBasico);
+            this.Controls.Add(this.TxtHoras);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TxtHoras;
+        private System.Windows.Forms.TextBox TxtBasico;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox TxtLegajo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox CboxCategoria;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbll;
+        private System.Windows.Forms.Label LblNetoaPagar;
+    }
+}
+
---
+++ empresa constructora/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace empresa_constructora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string Categoria;
+
+   
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+      
+
+            Categoria = CboxCategoria.Text;
+            switch (Categoria)
+            {
+                case "CategoriaA":
+                    
+                    double snetoa = 2.10 * Convert.ToDouble(TxtHoras.Text) + Convert.ToDouble(TxtBasico.Text);
+                    LblNetoaPagar.Text = Convert.ToString(snetoa) + "  PESOS";
+                    break;
+               
+                case "CategoriaB":
+
+                    double snetob = 2.30 * Convert.ToDouble(TxtHoras.Text) + Convert.ToDouble(TxtBasico.Text);
+                    LblNetoaPagar.Text = Convert.ToString(snetob)  + "  PESOS";
+                    break;
+
+                case "CategoriaC":
+
+                    double snetoc = 2.80 * Convert.ToDouble(TxtHoras.Text) + Convert.ToDouble(TxtBasico.Text);
+                    LblNetoaPagar.Text = Convert.ToString(snetoc) + "  PESOS";
+                    break;
+
+             }
+           
+        }
+
+   
+    }
+}
